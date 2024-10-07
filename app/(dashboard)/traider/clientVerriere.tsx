01c78d1ee4f6b65284e1c2ef14a9e9800456121a
Ahmed Schhaider <ahmed.schhaider@gmail.com>
2024-10-07 13:27:19 +0100

    AS: adding the requiered pages for the dashboard

---
+++ app/(dashboard)/traider/clientVerriere.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const clientVerriere = () => {
+  return (
+    <View>
+      <Text>clientVerriere</Text>
+    </View>
+  );
+};
+
+export default clientVerriere;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import Login from "./(auth)/login";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -13,7 +18,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -27,9 +32,60 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)/login" />
+        <Stack.Screen name="(auth)/register" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/IncomingClients" />
+        <Stack.Screen name="(dashboard)/ReservationChamber" />
+        <Stack.Screen name="(dashboard)/traider/client" />
+        <Stack.Screen name="(dashboard)/traider/clientVerriere" />
+        <Stack.Screen name="(dashboard)/traider/company" />
+        <Stack.Screen name="(dashboard)/traider/doneReservations" />
+        <Stack.Screen name="(dashboard)/traider/sellers" />
+        <Stack.Screen name="(dashboard)/traider/sellersState" />
+        <Stack.Screen name="(dashboard)/restaurant/daylyCloseTickets" />
+        <Stack.Screen name="(dashboard)/restaurant/closedTickets" />
+        <Stack.Screen name="(dashboard)/restaurant/settings/plateOptions" />
+        <Stack.Screen name="(dashboard)/restaurant/settings/platesFormulas" />
+        <Stack.Screen name="(dashboard)/restaurant/settings/tables" />
+        <Stack.Screen name="(dashboard)/economa/articles" />
+        <Stack.Screen name="(dashboard)/economa/incomingGoods" />
+        <Stack.Screen name="(dashboard)/economa/inventaire" />
+        <Stack.Screen name="(dashboard)/economa/statistics" />
+        <Stack.Screen name="(dashboard)/economa/transferGoods" />
+        <Stack.Screen name="(dashboard)/economa/settings/articleCategories" />
+        <Stack.Screen name="(dashboard)/economa/settings/articleSubCategories" />
+        <Stack.Screen name="(dashboard)/economa/settings/articleType" />
+        <Stack.Screen name="(dashboard)/economa/settings/lot" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
+        <Stack.Screen name="(dashboard)/index" />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
